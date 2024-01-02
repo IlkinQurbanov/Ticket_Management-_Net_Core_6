ea67b87728f4d9bbaf9d93c8150d49445a935129
IlkinQurbanov <ilkin.qurbanli.92@inbox.ru>
2024-01-02 22:24:26 +0400

    Ticket Management system

---
+++ TicketManagement.Domain/Entities/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketManagement.Domain.Entities
+{
+    public class Category
+    {
+        public Guid CategoryOd { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public ICollection<Event>? Events { get; set; }
+
+    }
+}
