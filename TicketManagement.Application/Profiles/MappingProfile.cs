fd6a7f33f1b6aa87d9e5bf7a549f3bb40f4983fa
IlkinQurbanov <ilkin.qurbanli.92@inbox.ru>
2024-01-02 23:56:06 +0400

    Create Mapping files and Handler

---
+++ TicketManagement.Application/Profiles/MappingProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TicketManagement.Application.Features.Events;
+using TicketManagement.Domain.Entities;
+
+namespace TicketManagement.Application.Profiles
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Event, EventListVm>().ReverseMap();
+            CreateMap<Event, EventDetailVm>().ReverseMap();
+            CreateMap<Category, CategoryDto>();
+
+        }
+    }
+}
